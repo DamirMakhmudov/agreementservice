183aa806df9f0fba75d5239e311b2587d4565345
Damir <d.makhmudov@kazan.csoft.ru>
2022-04-26 16:24:00 +0300

    last

--- position.js
+++ position.js
@@ -3,14 +3,31 @@ app.component('position', {
   template:
     /*html*/
     `
-    <div class="q-pa-md row fit" >
-      <q-field label="Адрес" stack-label :dense="true" class="q-pr-md" style="max-width: 70%;">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{cAddress}}</div>
-      </template>
-    </q-field>
-    </div>
-  `
+<div class="q-pa-md row fit items-end">
+  <q-field label="Исполнитель" dark stack-label borderless :dense="true" class="q-pr-md" style="width: 15%;">
+    <template v-slot:prepend>
+      <q-icon name="person" />
+    </template>
+    <template v-slot:control >
+      <div class="self-center full-width no-outline">{{cEmployee}}</div>
+    </template>
+  </q-field>
+
+  <q-field label="Телефон" dark stack-label borderless :dense="true" class="q-pr-md" style="width: 10%;">
+    <template v-slot:prepend>
+      <q-icon name="place" />
+    </template>
+    <a :href="'tel:'+users.phones[users.names.indexOf(cEmployee)]">{{users.phones[users.names.indexOf(cEmployee)]}}</a>
+  </q-field>
+
+  <q-field label="Email" dark stack-label borderless :dense="true" class="q-pr-md" style="width: 10%;">
+    <template v-slot:prepend>
+      <q-icon name="email" />
+    </template>
+    <a :href="'mailto:'+users.emails[users.names.indexOf(cEmployee)]">{{users.emails[users.names.indexOf(cEmployee)]}}</a>
+  </q-field>
+</div>
+`
   ,
   props: {
     cAddress: String,
@@ -18,8 +35,10 @@ app.component('position', {
     cStatus: String
   },
   setup(props) {
+    let users = reactive(model.users);
+ 
     return {
-
+      users
     }
   }
 })
--- root.js
+++ root.js
@@ -1,75 +1,83 @@
 const { createApp, ref, reactive, computed, watch, onMounted, watchEffect, onBeforeUnmount } = Vue;
-const { useQuasar, Loading, QSpinnerGears } = Quasar;
+const { Platform, useQuasar, Loading, QSpinnerGears } = Quasar;
 
 var vueObject = {
-  name: 'root',
-  template:
-    /*html*/
-    `
-<!-- <div class="q-pa-md"> -->
-  <div class="q-pa-md row fit" >
-    
-  <div v-for='item in modelc.addresses'>
-    <position :cAddress='item.address' :cEmployee='item.employee' :cStatus='item.status'></position>
-  </div>
-
-    <!-- <q-field label="Адрес" stack-label :dense="true" class="q-pr-md" style="max-width: 70%;">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{address}}</div>
-      </template>
-    </q-field>
-
-    <q-field label="Исполнитель" stack-label :dense="true" class="q-pr-md">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{employee}}</div>
-      </template>
-    </q-field>
-
-    <q-field label="Статус" stack-label :dense="true" class="q-pr-md">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{status}}</div>
-      </template>
-    </q-field>
-  </div>
-
-  <div class="q-pa-md row fit">
-    <q-field label="Адрес" stack-label :dense="true" class="q-pr-md">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{address}}</div>
-      </template>
-    </q-field>
-
-    <q-field label="Исполнитель" stack-label :dense="true" class="q-pr-md">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{employee}}</div>
-      </template>
-    </q-field>
-
-    <q-field label="Статус" stack-label :dense="true" class="q-pr-md">
-      <template v-slot:control>
-        <div class="self-center full-width no-outline">{{status}}</div>
-      </template>
-    </q-field>
-  </div> -->
-</div>
-
+name: 'root',
+template:
+/*html*/
 `
-  ,
-  setup() {
-    let modelc = reactive(model)
+<q-list bordered class="rounded-borders bg-grey-10" separator>
 
-    return {
-      modelc
-    }
-  }
+  <q-expansion-item dark v-for="item in modelc.addresses" expand-separator icon="place" :label="item.address"
+    expand-icon-toggle :caption="item.status">
+
+    <template v-slot:header>
+      <q-item-section>
+        <div class="row fit items-center">
+          <q-icon name="place" size="26px" color="grey"></q-icon>
+          <q-field dark color="white" label="Адрес" stack-label borderless :dense="false">
+            {{item.address}}
+          </q-field>
+        </div>
+      </q-item-section>
+
+      <q-item-section>
+        <div class="row fit items-center">
+          <q-icon name="info" size="26px" color="grey"></q-icon>
+          <q-field dark :color="item.color" label="Статус" stack-label borderless :dense="false">
+            {{item.status}}
+          </q-field>
+        </div>
+      </q-item-section>
+
+      <q-item-section side>
+        <q-btn flat color="primary" icon="verified" @click="confirm = true">Утвердить план</q-btn>
+      </q-item-section>
+    </template>
+
+    <position :cAddress='item.address' :cEmployee='item.employee' :cStatus='item.status'></position>
+
+  </q-expansion-item>
+
+</q-list>
+
+<q-dialog v-model="confirm" persistent>
+  <q-card>
+    <q-card-section class="row items-center">
+      <q-avatar icon="contact_support" color="primary" text-color="white" />
+      <span class="q-ml-sm">Вы уверены?</span>
+    </q-card-section>
+
+    <q-card-actions align="right">
+      <q-btn flat label="Да" color="primary" v-close-popup @click="changeStatus()"></q-btn>
+      <q-btn flat label="Нет" color="primary" v-close-popup></q-btn>
+    </q-card-actions>
+  </q-card>
+</q-dialog>
+`
+,
+setup() {
+let modelc = reactive(model)
+let address = ref(model.address)
+let employee = ref(model.employee)
+let status = ref(model.status)
+const $q = useQuasar()
+
+function changeStatus() {
+console.log($q.platform.is.desktop)
+}
+return {
+modelc, address, employee, status, changeStatus, confirm: ref()
+}
+}
 }
 
 const app = Vue.createApp(vueObject);
 app.use(Quasar, {
-  config: {
-    notify: { /* look at QuasarConfOptions from the API card */ },
-    loading: { /* look at QuasarConfOptions from the API card */ }
-  }
+config: {
+notify: { /* look at QuasarConfOptions from the API card */ },
+loading: { /* look at QuasarConfOptions from the API card */ }
+}
 });
 
 Quasar.lang.set(Quasar.lang.ru);

695c73352affea06447fb2d785cc686dca3ef58d
Damir <d.makhmudov@kazan.csoft.ru>
2022-07-16 19:16:59 +0300

    new ver

--- position.js
+++ position.js
@@ -1,49 +1,90 @@
 app.component('position', {
-  name: "position",
-  template:
-    /*html*/
-    `
-<div class="q-pa-md row fit items-end" v-if="cEngineer != ''" >
+name: "position",
+template:
+/*html*/
+`
+<!-- <div class="q-pa-md row fit items-end" v-if="cEngineer != ''" > -->
 
-  <q-field dark :label="cPosition"  stack-label borderless :dense="true" class="q-pr-md" style="width: 15%">
-    <template v-slot:prepend>
-      <q-icon name="person" />
-    </template>
-    <template v-slot:control>
-      <div class="self-center full-width no-outline">{{cEngineer}}</div>
-    </template>
-  </q-field>
+<div class="q-pa-md row" >
+  <!-- <div class="q-gutter-md" style="max-width: 300px"> -->
 
-  <q-field dark label="Телефон"  stack-label borderless :dense="true" class="q-pr-md" style="width: 10%;">
-    <template v-slot:prepend>
-      <q-icon name="place" />
-    </template>
-    <a
-      :href="'tel:'+engineers.phones[engineers.names.indexOf(cEngineer)]">{{engineers.phones[engineers.names.indexOf(cEngineer)]}}</a>
-  </q-field>
+  <div v-if="cEngineer != ''">
 
-  <q-field dark label="Email"  stack-label borderless :dense="true" class="q-pr-md" style="width: 10%;">
-    <template v-slot:prepend>
-      <q-icon name="email" />
-    </template>
-    <a
-      :href="'mailto:'+engineers.emails[engineers.names.indexOf(cEngineer)]">{{engineers.emails[engineers.names.indexOf(cEngineer)]}}</a>
-  </q-field>
+    <q-field label="Инженер" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="person" />
+      </template>
+      <template v-slot:control>
+        <div class="self-center full-width no-outline">{{cEngineer}}</div>
+      </template>
+    </q-field>
+
+    <q-field label="Телефон" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="phone" />
+      </template>
+      <a
+        :href="'tel:'+engineers.phones[engineers.names.indexOf(cEngineer)]">{{engineers.phones[engineers.names.indexOf(cEngineer)]}}</a>
+    </q-field>
+
+    <q-field label="Email" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="email" />
+      </template>
+      <a
+        :href="'mailto:'+engineers.emails[engineers.names.indexOf(cEngineer)]">{{engineers.emails[engineers.names.indexOf(cEngineer)]}}</a>
+    </q-field>
+
+    <!-- </div> -->
+  </div>
+
+
+  <div v-if="cMeasurer != ''">
+
+    <q-field label="Обмерщик" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="person" />
+      </template>
+      <template v-slot:control>
+        <div class="self-center full-width no-outline">{{cMeasurer}}</div>
+      </template>
+    </q-field>
+
+    <q-field label="Телефон" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="phone" />
+      </template>
+      <a
+        :href="'tel:'+measurers.phones[measurers.names.indexOf(cMeasurer)]">{{measurers.phones[measurers.names.indexOf(cMeasurer)]}}</a>
+    </q-field>
+
+    <q-field label="Email" stack-label borderless :dense="true" class="q-pr-md">
+      <template v-slot:prepend>
+        <q-icon name="email" />
+      </template>
+      <a
+        :href="'mailto:'+measurers.emails[measurers.names.indexOf(cMeasurer)]">{{measurers.emails[measurers.names.indexOf(cMeasurer)]}}</a>
+    </q-field>
+
+    <!-- </div> -->
+  </div>
 
 </div>
+
 `
-  ,
-  props: {
-    cAddress: String,
-    cEngineer: String,
-    cPosition: String
-  },
-  setup(props) {
-    let engineers = reactive(model.engineers);
-    let measurers = reactive(model.measurers);
-    return {
-      engineers,
-      measurers
-    }
-  }
+,
+props: {
+cAddress: String,
+cEngineer: String,
+cMeasurer: String,
+cPosition: String
+},
+setup(props) {
+let engineers = reactive(model.engineers);
+let measurers = reactive(model.measurers);
+return {
+engineers,
+measurers
+}
+}
 })
--- root.js
+++ root.js
@@ -9,105 +9,14 @@ template:
 `
 <q-layout view="hHh lpR fFf">
   <q-page-container>
-
-        <!-- <div class="q-pa-md">
-          <q-list bordered class="rounded-borders">
-            <q-expansion-item
-              expand-separator
-              icon="perm_identity"
-              label="Account settings"
-              caption="John Doe"
-            >
-              <q-card>
-                <q-card-section>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                  eveniet doloribus ullam aliquid.
-                </q-card-section>
-              </q-card>
-            </q-expansion-item>
-      
-            <q-expansion-item
-              expand-separator
-              icon="signal_wifi_off"
-              label="Wifi settings"
-            >
-              <q-card>
-                <q-card-section>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                  eveniet doloribus ullam aliquid.
-                </q-card-section>
-              </q-card>
-            </q-expansion-item>
-      
-            <q-expansion-item
-              expand-separator
-              icon="drafts"
-              label="Drafts"
-              header-class="text-purple"
-            >
-              <q-card>
-                <q-card-section>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                  eveniet doloribus ullam aliquid.
-                </q-card-section>
-              </q-card>
-            </q-expansion-item>
-      
-            <q-expansion-item icon="assessment" label="Disabled" disable>
-              <q-card>
-                <q-card-section>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-                  eveniet doloribus ullam aliquid.
-                </q-card-section>
-              </q-card>
-            </q-expansion-item>
-          </q-list>
-        </div> -->
-
+    <div>
     <q-list bordered class="rounded-borders">
-      <!-- <q-list bordered class="rounded-borders bg-grey-10" > -->
-
-      <q-expansion-item v-for="item in modelc.addresses" expand-separator icon="place" :label="item.address" expand-icon-toggle :caption="item.status" header-class="mystyle">
-
-        <template v-slot:header class="text-h4">
-      <q-item-section >
-      <div class="column  items-left q-col-gutter-xl">
-
-        <div class="row  items-center ">
-          <q-icon name="place" size="26px" color="grey"></q-icon>
-          <q-field dark color="primary" label="Адрес" stack-label borderless :dense="false" >
-            {{item.address}}
-          </q-field>
-        </div>
-
-        <!-- </q-item-section> -->
-
-        <!-- <q-item-section> -->
-        <!-- <div class="row items-center">
-          <q-icon name="info" size="26px" color="grey"></q-icon>
-          <q-field dark color="primary" label="Статус" stack-label borderless :dense="false">
-            {{item.status}}
-          </q-field>
-        </div>
-      </div> -->
-
-        </q-item-section>
-
-        <q-item-section side>
-            <q-btn flat color="primary" icon="verified" @click="confirm = true">Утвердить план</q-btn>
-          </q-item-section>
-        <!-- </template> -->
-
-        <!-- <position :cAddress='item.address' :cEngineer='item.engineer' cPosition='Инженер'></position> -->
+      <q-expansion-item :header-class="item.style" class="shadow-1 overflow-hidden q-my-xs"  style="border-radius: 30px" :dense="true" v-for="item in modelc.addresses" expand-separator icon="place" :label="item.address" expand-icon-toggle :caption="item.status">
+        <position :cAddress='item.address' :cEngineer='item.engineer' cPosition='Инженер' :cMeasurer='item.measurer'></position>
         <!-- <position :cAddress='item.address' :cEngineer='item.measurer' cPosition='Обмерщик'></position> -->
-
       </q-expansion-item>
-
     </q-list>
+</div>
   </q-page-container>
 </q-layout>
 
